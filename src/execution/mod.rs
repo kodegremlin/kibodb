ddf682dd500ffeed22e13cff0a26c30df69036ec
Parth Srivastav <parthsrivastav.00@gmail.com>
2026-08-06 21:35:52 +0530

    feat: added sequential scan executor enabling table wide scans

--- src/execution/mod.rs
+++ src/execution/mod.rs
@@ -1,2 +1,5 @@
+pub(crate) mod evaluator;
 pub(crate) mod executor;
+pub(crate) mod filter;
 pub(crate) mod iterator;
+pub(crate) mod seq_scan;
---
+++ src/execution/seq_scan.rs
@@ -0,0 +1,38 @@
+use std::io::Cursor;
+
+use crate::{
+    error::Error,
+    execution::{
+        executor::{ExecutionContext, Executor},
+        iterator::BpTreeIterator,
+    },
+    relation::{schema::Schema, tuple::Tuple},
+};
+
+/// The logical iterator that performs a full table scans.
+#[derive(Debug)]
+pub struct SeqScanExecutor {
+    iterator: BpTreeIterator,
+    schema: Schema,
+}
+
+impl SeqScanExecutor {
+    /// Initializes a new sequential scan executor.
+    pub fn new(iterator: BpTreeIterator, schema: Schema) -> Self {
+        Self { iterator, schema }
+    }
+}
+
+impl Executor for SeqScanExecutor {
+    fn next(&mut self, ctx: &mut ExecutionContext) -> Result<Option<Tuple>, Error> {
+        if !self
+            .iterator
+            .next(ctx.buffer_pool, &mut ctx.buffer_block)?
+        {
+            return Ok(None);
+        }
+        let mut cursor = Cursor::new(&ctx.buffer_block);
+        let tuple = Tuple::decode(&self.schema, &mut cursor)?;
+        Ok(Some(tuple))
+    }
+}
