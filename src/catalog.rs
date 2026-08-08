cb3b4b099d066a68c9ac97a60064c13010ba4d03
Parth Srivastav <parthsrivastav.00@gmail.com>
2026-08-08 07:49:53 +0530

    add: sys index schema

--- src/catalog.rs
+++ src/catalog.rs
@@ -25,3 +25,15 @@ pub fn sys_schema_schema() -> Schema {
         Column::new("field_length", DataType::Int, None),
     ])
 }
+
+/// Generates the schema for `sys_index` catalog.
+/// Tracks secondary indexes mapped to base tables.
+pub fn sys_index_schema() -> Schema {
+    Schema::new(vec![
+        Column::new("table_name", DataType::Varchar, Some(255)),
+        Column::new("index_name", DataType::Varchar, Some(255)),
+        Column::new("column_name", DataType::Varchar, Some(255)),
+        Column::new("root_page_id", DataType::BigInt, None),
+        Column::new("is_unique", DataType::Boolean, None),
+    ])
+}
